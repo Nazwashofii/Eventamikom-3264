a46bf37cf1e6478f4ce79957d0134d14aa745e45
nazwa <nazwaazzhrashof@students.amikom.ac.id>
2026-04-12 11:50:08 +0700

    Tugas 1

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/profil', function () {
     return view('profil'); // atau bisa ke halaman lain
 });
 Route::get('/tentang', function () {
