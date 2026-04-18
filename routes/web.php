5ddb1c26eed1f5201af19f3ab185c58cf1de7e2c
nazwa <nazwaazzhrashof@students.amikom.ac.id>
2026-04-18 21:09:22 +0700

    Tugas 2

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,30 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\Admin\DashboardController;
+
+// Rute User Area
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/event/1', [EventController::class,
+'show'])->name('events.show');
+Route::get('/checkout', [EventController::class,
+'checkout'])->name('checkout');
+Route::get('/my-ticket', [EventController::class, 'ticket'])->name('ticket');
+
+// Rute Admin Area
+Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
+
+Route::get('/', [DashboardController::class,
+
+'index'])->name('dashboard');
+
+Route::get('/events', [EventController::class,
+
+'indexAdmin'])->name('events.index');
+// dan seterusnya...
+});
 
 Route::get('/', function () {
     return view('welcome');
@@ -32,4 +56,16 @@ Route::get('/dashboard', function () {
 
 Route::get('/events', function () {
     return view('admin.events');
+});
+Route::get('/transaksi', function () {
+    return view('admin.transactions');
+});
+Route::get('/tiket', function () {
+    return view('ticket');
+});
+Route::get('/checkout', function () {
+    return view('checkout');
+});
+Route::get('/event-detail', function () {
+    return view('event-detail');
 });
